fe90b33893e22962f2cc009ec5d18d91c2fd17c6
John Delano <jdelano@cedarville.edu>
2020-03-23 23:06:10 -0400

    Add collision detection via LINQ

--- Derby/Game.cs
+++ Derby/Game.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Derby
 {
     public class Game
     {
-        private int numberOpponents = 8;
+        private int numberOpponents = 2;
         public Car Player { get; set; }
         public List<Car> Opponents { get; set; }
 
@@ -21,7 +22,7 @@ namespace Derby
                 opponent.StartEngine();
                 Opponents.Add(opponent);
             }
-            
+
         }
 
         private bool isPlaying = true;
@@ -55,14 +56,38 @@ namespace Derby
         private void UpdateGame()
         {
             int updateInterval = 2000; // 1/2 a second
-            if (DateTime.Now.Subtract(gameTime) > 
+            if (DateTime.Now.Subtract(gameTime) >
                 TimeSpan.FromMilliseconds(updateInterval))
             {
                 for (int i = 0; i < Opponents.Count; i++)
                 {
                     Opponents[i].MakeRandomMovement();
                 }
-                
+                //for (int i = Opponents.Count - 1; i >= 0; i--)
+                //{
+                //    if (Player.LocationX == Opponents[i].LocationX &&
+                //           Player.LocationY == Opponents[i].LocationY)
+                //    {
+                //        // Player collides with Opponent
+                //        Opponents.Remove(Opponents[i]);
+                //    }
+                //}
+
+                var crashes = from car in Opponents
+                              where car.LocationX == Player.LocationX &&
+                              car.LocationY == Player.LocationY
+                              select car;
+
+                var crashList = crashes.ToList();
+                foreach (var car in crashList)
+                {
+                    Opponents.Remove(car);
+                }
+
+                if (Opponents.Count == 0)
+                {
+                    isPlaying = false;
+                }
                 invalidated = true;
                 gameTime = DateTime.Now;
             }
